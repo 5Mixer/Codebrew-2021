ef9e29aaad9e6f12b7ee3337b7e97f3e9e029b0b
Henrique Pureza <henriquepureza13@gmail.com>
2021-04-17 15:44:08 +1000

    Add simple express server

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+// app.get("/api/products", (req, res) => {
+//   res.send(data.products);
+// });
+
+app.get("/", (req, res) => {
+  res.send("Server is Running");
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
