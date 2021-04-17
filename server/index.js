3da89cdc418224832c4ab6da56c4f4997b858967
Henrique Pureza <henriquepureza13@gmail.com>
2021-04-17 16:24:05 +1000

    Add save blocky api with mongodb

--- server/index.js
+++ server/index.js
@@ -1,13 +1,28 @@
 import express from "express";
+import mongoose from "mongoose";
+import blocklyRouter from "./routers/blocklyRouter.js"
+
 
 const app = express();
 
-// app.get("/api/products", (req, res) => {
-//   res.send(data.products);
-// });
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+app.post("/api/createBot", (req, res) => {
+  res.send("This is the api for creating bots");
+});
+
+app.use('api/blocklys', blocklyRouter)
 
 app.get("/", (req, res) => {
   res.send("Server is Running");
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
