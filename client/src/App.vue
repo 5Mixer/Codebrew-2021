56ff893dd1aeb6ce60012b8fbeb0e3cbe12f9970
Daniel B <dantheman41@live.com.au>
2021-04-17 13:30:26 +1000

    Wip Vue transition

---
+++ client/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+
+    <table>
+      <tr>
+        <td>
+          <h1>Block based bot editor</h1>
+        </td>
+      </tr>
+      <tr>
+        <td id="blocklyArea">
+        </td>
+      </tr>
+    </table>
+    
+    <div id="blocklyDiv" style="position: absolute"></div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    
+
+    <HelloWorld msg="Hello Vue 3 + Vite" />
+
+  </div>
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+
+</script>
