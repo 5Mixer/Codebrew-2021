c56b9ed31427aab5525316db425445da86e2fca3
Henrique Pureza <henriquepureza13@gmail.com>
2021-04-18 03:54:23 +1000

    Fix user model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -5,8 +5,8 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: false },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    slackToken: { type: String, required: false},
-    discordToken: { type: String, required: false},
+    slackToken: { type: String, default: null, required: false},
+    discordToken: { type: String, default: null, required: false},
   },
   {
     timestamps: true,
