29dbcf8153c015099a4c68e5f716de16bc2faa92
Daniel B <dantheman41@live.com.au>
2021-04-17 18:11:55 +1000

    Post block js

--- client/src/App.vue
+++ client/src/App.vue
@@ -22,21 +22,16 @@ import {toolbox} from './blockly/toolbox';
 
 import BlocklyJS from 'blockly/javascript';
 
-async function postData(url = '', data = {}) {
+async function postData(url = '', data) {
   // Default options are marked with *
   const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'no-cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
+    method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: data // body data type must match "Content-Type" header
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
   });
-  return response.json(); // parses JSON response into native JavaScript objects
+  return response; // parses JSON response into native JavaScript objects
 }
 
 export default {
@@ -64,7 +59,11 @@ export default {
     showCode() {
       this.code = BlocklyJS.workspaceToCode(this.$refs["foo"].workspace);
 
-      postData('/api/createBot', this.code);
+      postData('http://localhost:5005/api/createBot', {
+        js: encodeURI(this.code)
+      }).catch((error) => {
+        console.log(error)
+      });
     }
   }
 }
--- server/index.js
+++ server/index.js
@@ -5,15 +5,18 @@ import blocklyRouter from "./routers/blocklyRouter.js"
 
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+// mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+// });
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
 app.post("/api/createBot", (req, res) => {
-  console.log(res.body);
-  res.send(res.body);
+  console.log(req.body);
+  res.status(200).end();
 });
 
 app.use('api/blocklys', blocklyRouter)
@@ -26,7 +29,7 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5005;
 
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
