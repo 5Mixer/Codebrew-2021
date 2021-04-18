11757f8910f8c66cf1eb816716c3a122bff9c880
Sousf <vopeter1662000@gmail.com>
2021-04-18 12:22:32 +1000

    About page

---
+++ client/src/components/About.vue
@@ -0,0 +1,136 @@
+<template>
+  <transition name="modal-fade">
+    <div class="modal-backdrop">
+      <div class="modal">
+        <header class="modal-header">
+          <slot name="header">
+            About us
+          </slot>
+          <button type="button" class="btn-close" @click="close">
+            X
+          </button>
+        </header>
+
+        <section class="modal-body">
+          <slot name="body">
+            <form class="form">
+              <div>
+                <label for="slackToken">YEEE BOOOOIII</label>
+              </div>
+              <div>
+                <label for="discordToken">YEEE BOOOOIII</label>
+              </div>
+            </form>
+          </slot>
+        </section>
+      </div>
+    </div>
+  </transition>
+</template>
+
+
+
+<script>
+
+export default {
+  name: "Modal",
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style>
+.modal-backdrop {
+  position: fixed;
+  padding: 25px;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal {
+  background: #ffffff;
+  box-shadow: 2px 2px 20px 1px;
+  overflow-x: auto;
+  height: 43%;
+  width: 30%;
+  display: flex;
+  flex-direction: column;
+}
+
+.modal-header,
+.modal-footer {
+  padding: 15px;
+  display: flex;
+}
+
+.modal-header {
+  position: relative;
+  color: #4aae9b;
+  justify-content: space-between;
+}
+
+.modal-footer {
+  flex-direction: column;
+  justify-content: flex-end;
+}
+
+.modal-body {
+  position: relative;
+  padding: 20px 10px;
+}
+
+.btn-close {
+  position: absolute;
+  top: 0;
+  right: 0;
+  border: none;
+  font-size: 20px;
+  padding: 10px;
+  cursor: pointer;
+  font-weight: bold;
+  color: #4aae9b;
+  background: transparent;
+}
+
+.btn-green {
+  color: white;
+  background: #4aae9b;
+  border: 1px solid #4aae9b;
+  border-radius: 2px;
+}
+.btn-red {
+  color: white;
+  background: red;
+  border: 1px solid #4aae9b;
+  border-radius: 2px;
+}
+.form {
+  max-width: 60rem;
+  margin: 0 auto;
+}
+
+.form > div {
+  display: flex;
+  flex-direction: column;
+  margin: 1rem;
+}
+
+.modal-fade-enter,
+.modal-fade-leave-to {
+  opacity: 0;
+}
+
+.modal-fade-enter-active,
+.modal-fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+</style>
